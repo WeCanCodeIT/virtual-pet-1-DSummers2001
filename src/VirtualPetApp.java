b3e1e6a6fd04ea922efcb54ebf7fc1907a4d8733
DSummers2001 <dillon.summers2001@gmail.com>
2023-12-03 12:38:01 -0800

    finished button commands now i need to test them

--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -32,18 +32,83 @@ public class VirtualPetApp {
             } 
             else if (select == 0){
                 if (dog.getAttention() < 5){
-                    System.out.println("You might need to show more love to " + dogName + "!");
+                    System.out.println("You need to show more love to " + dogName + "!");
                     continue;
                 }
                 System.out.println(dogName + " Really enjoyed that walk!");
                 dog.changeAttention(+1);
                 dog.changeEnergy(-1);
                 dog.changeHygiene(-1);
+                showDogCurrentNeeds(dog);
             }
             else if ( select == 1){
-                
+                if (dog.getHunger() > 5){
+                    System.out.println(dogName + " isn't hungry right now.");
+                    continue;
+                }
+                System.out.println(dogName + " Loved that treat and wags his tail happily!");
+                dog.changeHunger(+3);
+                dog.changeAttention(-1);
+                dog.changeEnergy(-1);
+                showDogCurrentNeeds(dog);
             }
-
+            else if (select == 2){
+                if (dog.getThirst() > 5){
+                    System.out.println(dogName + " does'nt want any water right now");
+                    continue;
+                }
+                System.out.println(dogName + " really enjoyed that water and thanks you with a happy bark!");
+                dog.changeThirst(+3);
+                dog.changeEnergy(-1);
+                dog.changeAttention(-1);
+                showDogCurrentNeeds(dog);
+            }
+            else if (select == 3){
+                if (dog.getAttention() > 5){
+                    System.out.println(dogName + " does'nt want to play right now.");
+                    continue;
+                }
+                dog.changeAttention(+1);
+                dog.changeEnergy(-1);
+                dog.changeHunger(-1);
+                dog.changeThirst(-1);
+                showDogCurrentNeeds(dog);
+            }
+            else if (select == 4){
+                if (dog.getBladder() > 5){
+                    System.out.println(dogName + " does'nt need to go to the bathroom yet.");
+                    continue;
+                }
+                System.out.println(dogName + " is really happy you let him out to use the bathroom!");
+                dog.changeBladder(+2);
+                dog.changeHygiene(-2);
+                showDogCurrentNeeds(dog);
+            }
+            else if (select == 5){
+                if (dog.getEnergy() > 5){
+                    System.out.println("Rest is not needed for " + dogName + " right now.");
+                    continue;
+                }
+                System.out.println(dogName + " Enjoyed his nap a lot and i wagging his tail happily!");
+                dog.changeEnergy(+3);
+                dog.changeHunger(-1);
+                dog.changeThirst(-1);
+                dog.changeAttention(-1);
+                showDogCurrentNeeds(dog);
+            }
+            else if (select == 6){
+                if (dog.getHygiene() > 5){
+                    System.out.println("Your " + dogName + " still has a clean and shiny coat!");
+                    continue;
+                }
+                System.out.println(dogName + " Enjoyed his grooming a lot is jumping in excitement");
+                dog.changeHygiene(+2);
+                dog.changeHunger(-1);
+                dog.changeThirst(-1);
+                showDogCurrentNeeds(dog);
+            } else {
+                continue;
+            }
 
             dog.tick();
         } while (select != 7);
